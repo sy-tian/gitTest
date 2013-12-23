a4826b58b525374f52e779005e6c96b1faabf623
angela <1911165211@qq.com>
2013-12-23 15:26:27 +0800

    exclude other unit

---
+++ src/main/java/com/thoughtworks/practice/WrongUnitException.java
@@ -0,0 +1,11 @@
+package com.thoughtworks.practice;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: admin
+ * Date: 13-12-23
+ * Time: 下午3:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class WrongUnitException {
+}
